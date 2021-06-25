46e38d0c7b0af1aea04132ba7220046852cce458
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-25 16:46:31 -0500

    (5) save and load in json

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new()  # aun no sabemos
+            storage.new(self)  # aun no sabemos
+
         # if kwargs have values
         if len(kwargs) > 0:
             for key, value in kwargs.items():
@@ -49,6 +50,7 @@ class BaseModel:
         """updates the public instance attribute updated_at with the
         current datetime"""
         self.updated_at = datetime.now()
+        print(self)
         storage.save()  # NOsabemos
 
     def to_dict(self):
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,6 +5,8 @@ the process the serializes and deserializes to JSON
 import json
 
 
+
+
 class FileStorage:
     """"""
     __file_path = 'file.json'
@@ -16,10 +18,37 @@ class FileStorage:
 
     def new(self, obj):
         key_name = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key_name]
+        FileStorage.__objects[key_name] = obj
 
     def save(self):
-        pass
+        content = self.__serialize()
+
+        with open(FileStorage.__file_path, 'w') as file:
+            file.write(content)
 
     def reload(self):
-        pass
+        from models.base_model import BaseModel
+        file_data = self.__deserialize()
+
+        if not file_data:
+            return
+        for k, v in file_data.items():
+            if v['__class__'] == 'BaseModel':
+                FileStorage.__objects[k] = BaseModel(**v)
+
+    def __serialize(self):
+        """
+        BaseModel->to_dict() -> <class 'dict'> -> JSON dump -> <class 'str'>
+        """
+
+        for key, obj in self.all().items():
+            FileStorage.__objects[key] =  obj.to_dict()
+
+        return str(json.dumps(self.all()))
+
+    def __deserialize(self):
+        try:
+            with open(FileStorage.__file_path) as file:
+                return json.load(file)
+        except:
+            pass
