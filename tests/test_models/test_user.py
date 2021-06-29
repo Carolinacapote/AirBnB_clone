50278c042d97c849b411c0bddcc4097c51d8c234
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-29 16:12:50 -0500

    add test to user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    This is a module test from BaseModel class and your methods.
+"""
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+        self.user = User()
+
+    def test_creation(self):
+        '''this test validate that creation proccess was correct.
+        '''
+        self.assertEqual(self.user.first_name, '')
