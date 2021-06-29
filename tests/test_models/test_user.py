07a6aebdcd5f67158da859aecd419dfefe70c535
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-29 16:32:22 -0500

    add all classes test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -17,17 +17,4 @@ class TestUser(unittest.TestCase):
     def test_creation(self):
         '''this test validate that creation proccess was correct.
         '''
-
-        # data = {'id' : 3,
-        #     'fist_name' : 'Betty',
-        #     'last_name':'Holberton',
-        #     'password':'123',
-        #     'email':'correo@correo',
-        #     }
-
-        # self.user = User(**data)
-        self.assertEqual(self.user.id, 3)
-        self.assertEqual(self.user.first_name, 'Betty')
-        self.assertEqual(self.user.first_name, 'Holberton')
-        self.assertEqual(self.user.password, '123')
-        self.assertEqual(self.user.email, 'correo@correo')
+        self.assertEqual(self.user.first_name, '')
